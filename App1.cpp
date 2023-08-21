7371d74c7bea12d3929832a6652ea7162efbc84e
jhyun1234 <anjyounl24@gmail.com>
2023-08-21 19:48:03 +0900

    2023.08.21 미로찾기

--- App1.cpp
+++ App1.cpp
@@ -1,8 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
-#define LEFT 75
-#define RIGHT 77
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,10 +7,22 @@
 #include <conio.h>
 #include <windows.h>
 
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
 
+#define WIDTH 11
+#define HEIGHT 11
 
+struct Player
+{
+	int x;
+	int y;
+	const char* shape;
+};
 
-
+// 좌표 이동 함수
 void gotoXY(int x, int y)
 {
 	// x, y 좌표 설정 
@@ -23,133 +32,132 @@ void gotoXY(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 
 }
-struct Player
+
+// 미로 맵 데이터
+
+char maze[WIDTH][HEIGHT];
+
+// 미로 맵 생성
+
+void CreateMaze()
 {
-	int x, y;
-	const char* shape;
-};
+	// 0 : 빈 공간 ("  ")
 
-struct Enemy
-{
-	int x, y;
-	const char* shape;
-};
-void Keyboard(Player* player)
-{
-	char key = 0;
+	// 1 : 벽 ("■")
 
-	if (_kbhit())
-	{
-		key = _getch();
+	// 2 : 탈출구 ("◎")
 
-		if (key == -32)
-		{
-			key = _getch();
-		}
+	strcpy(maze[0], "11111111");
+	strcpy(maze[1], "11110011");
+	strcpy(maze[2], "11000111");
+	strcpy(maze[3], "11010111");
+	strcpy(maze[4], "11010101");
+	strcpy(maze[5], "11100101");
+	strcpy(maze[6], "11001101");
+	strcpy(maze[7], "11010102");
+	strcpy(maze[8], "11000001");
+	strcpy(maze[9], "11111111");
 
-		switch (key)
-		{
-		case LEFT:if (player->x <= 0)return;
-			player->x -= 2;
-			break;
-		case RIGHT:if (player->x >= 28)return;
-			player->x += 2;
-			break;
-
-		}
-		
-	}
 }
 
-int RandomX()
+void Renderer()
 {
-	srand(time(NULL));
-	int x = rand() % 31;
+	
 
-	if (x % 2 == 1)
-	{
-		x += 1;
-	}
-
-	return x;
+	for (int i = 0; i < WIDTH; i++)
+    {
+	   for (int j = 0; j <HEIGHT; j++)
+	   {
+		   if (maze[i][j] == '0')
+		   {
+			   printf("  ");
+			}
+		   else if (maze[i][j] == '1')
+		   {
+			   printf("■");
+		   }
+		   else if (maze[i][j] == '2')
+		   {
+			   printf("◎");
+		   }
+	   }
+	   printf("\n");
+    }
 }
 
+void KeyBoard(char map[WIDTH][HEIGHT], Player * player)
+{
+	 char key = 0;
+	 int x = 5;
+	 int y = 5;
+	 
+	 //  Update()
+	 while(1)
+	 {
+	 	gotoXY(x, y);
+	 	printf("★");
+	 
+	 	if (_kbhit()) // 키보드 입력을 확인하는 함수
+	 	{
+	 
+	 		key = _getch();
+	 		if (key == -32)
+	 		{
+	 			key = _getch();
+	 		}
+	 
+	 
+	 		switch (key)
+	 		{
+			case UP: if (map[player->y - 1][player->x / 2] != '1')
+			{
+				player->y--;
+			}
+	 			break;
+			case LEFT:if (map[player->y][player->x / 2 - 1] != '1')
+			{
+				player->x -= 2;
+			}	
+	 			break;
+			case RIGHT:if (map[player->y][player->x / 2 + 1] != '1')
+			{
+				player->x += 2;
+			}				
+	 			break;
+			case DOWN:if (map[player->y + 1][player->x / 2] != '1')
+			{
+				player->y++;
+			}
+				
+	 			break;
+	 		
+	 		}
+	 		// 스크린을 지우는 함수
+	 		system("cls");
+	 	}
+	 	
+	 	// Sleep : 1/1000의 1초로 계산된다.
+	 
+	 }
 
-
+}
 int main()
 {
+	Player player = { 4,2, "★" };
 
-#pragma region 문자열 관련 함수
-
-	// 문자열 길이 함수
-	/*
-	const char* name = "James";
-	int result = strlen(name);
-	printf("resul의 값 : %d,", result);
-	*/
-
-	// 문자열 연결 함수
-	/*
-	char firstArr[20] = "First";
-	char secondArr[] = "Second";
-
-	strcat(firstArr, secondArr);
-	
-	printf("fisrtArr의 값 : %s\n", firstArr);
-	*/
-
-	// 문자열 복사 함수
-	/*char a[10] = {"String"};
-	char b[10];
-
-	// 첫 번째 매개변수 : 복사받을 문자 배열
-	// 두 번째 매개변수 : 복사할 문자 배열
-	strcpy(b, a);
-
-	printf("a의 문자열 : %s\n", a);
-	printf("b의 문자열 : %s\n", b);
-	*/
-
-	// 문자열 비교 함수
-	/*
-	char firstA[] = { "ABB" };
-	char SecondB[] = { "ABC" };
-     
-	// 서로 같으면 "0"
-	// 앞 쪽에 있는 값이 크면 "1"
-	// 뒤 쪽에 있는 값이 크면 "-1"
-
-	printf("두 문자열을 비교한 결과 : %d\n", strcmp(firstA, SecondB));
-	*/
-#pragma endregion
-
-	system("mod con cols=30 lines=25");
-	Player player = { 16,23,"◆" };
-	Enemy enemy = { RandomX(),0,"♣" };
-
+	CreateMaze();
 	while (1)
 	{
-		Keyboard(&player);
-
-		if (enemy.y >= 24)
-		{
-			enemy.y = 0;
-			enemy.x = RandomX();
-		}
-
-		if (player.x == enemy.x && player.y == enemy.y)
-		{
-			break;
-		}
-		gotoXY(enemy.x, enemy.y++);
-		printf("%s", enemy.shape);
+		Renderer();
+		KeyBoard(maze, &player);
 
 		gotoXY(player.x, player.y);
 		printf("%s", player.shape);
 
 		Sleep(100);
-			system("cls");
+		system("cls");
 	}
+	
 
 	return 0;
 }
